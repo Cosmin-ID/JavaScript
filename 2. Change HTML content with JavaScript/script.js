a665779c920cc81935b9f488e5f0808381e76d25
Cosmin-ID <157300816+Cosmin-ID@users.noreply.github.com>
2024-10-07 23:17:53 +0300

    Add files via upload

---
+++ 2. Change HTML content with JavaScript/script.js
@@ -0,0 +1,6 @@
+function changeText() {
+    // Targeting the HTML element id.
+    let heading = document.getElementById("text");
+    // Asigning new text to the HTML element 
+    heading.textContent = "New text";
+};
